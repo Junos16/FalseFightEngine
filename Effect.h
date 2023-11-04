f118ef7122aaf8d0bb06fbaf1a979113db24839a
Junos16 <hriddhitdatta2002@gmail.com>
2023-11-04 12:20:23 +0530

    create class for zones and player, add private attributes

--- Effect.h
+++ Effect.h
@@ -4,7 +4,8 @@
 class Effect {
     private:
         std::string effectType;
-        std::vector<Location> can_be_activated_from;
+        //std::vector<Location> can_be_activated_from;
+
         std::vector<std::string> keywords;
         std::vector<Effect> subEffects;
 

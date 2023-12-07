91f47d36dfa9ebae1c07636bfdc54b2e756c1e8e
Junos16 <hriddhitdatta2002@gmail.com>
2023-12-07 22:45:03 +0530

    Add public methods for Game.h and Game.cpp

--- Game.h
+++ Game.h
@@ -1,7 +1,19 @@
-#include <vector>
-#include <string>
-
+#include "Player.h"
+#include "Card.h"
 class Game {
     private:
-
+        Player* turn_player;
+        Player* non_turn_player;
+        Player* went_first_player;
+        Player* went_second_player;
+        
+    public:
+        Game();
+        void gameLoop();
+        void perform_stand_phase();
+        void perform_draw_phase();
+        void perform_ride_phase();
+        void perform_main_phase();
+        void perform_battle_phase();
+        void perform_end_phase();   
 };
